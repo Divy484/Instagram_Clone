23cbe12d3b5e7d9e3a322037ae4e009360d117fc
Divy484 <98522523+Divy484@users.noreply.github.com>
2023-10-04 15:07:08 +0530

    Instagram

---
+++ app/src/main/java/com/example/instagramclone/Models/User.kt
@@ -0,0 +1,15 @@
+package com.example.instagramclone.Models
+
+class User {
+    var mobileEmail: String? = null
+    var name: String? = null
+    var username: String? = null
+    var password: String? = null
+    constructor()
+    constructor(mobileEmail: String?, name: String?, username: String?, password: String?) {
+        this.mobileEmail = mobileEmail
+        this.name = name
+        this.username = username
+        this.password = password
+    }
+}
--- app/src/main/java/com/example/instagramclone/SignUpActivity.kt
+++ app/src/main/java/com/example/instagramclone/SignUpActivity.kt
@@ -5,16 +5,30 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import com.example.instagramclone.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    val binding by lazy {
+        ActivitySignUpBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(binding.root)
         window.statusBarColor = Color.TRANSPARENT
 
         val login : Button = findViewById(R.id.login_button)
         login.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.signupButton.setOnClickListener {
+            if (binding.mobileEmail.editText?.text.toString().equals("") or
+                binding.name.editText?.text.toString().equals("") or
+                binding.username.editText?.text.toString().equals("") or
+                binding.password.editText?.text.toString().equals("")) {
+                Toast.makeText(this@SignUpActivity, "Please fill all details", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
