d8b58bdfc513a571b4ef7eeceb80be06b6549792
Divy484 <98522523+Divy484@users.noreply.github.com>
2023-10-04 12:30:49 +0530

    Instagram

---
+++ app/src/main/java/com/example/instagramclone/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.example.instagramclone
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        window.statusBarColor = Color.TRANSPARENT
+    }
+}
--- app/src/main/java/com/example/instagramclone/SignUpActivity.kt
+++ app/src/main/java/com/example/instagramclone/SignUpActivity.kt
@@ -1,11 +1,20 @@
 package com.example.instagramclone
 
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        window.statusBarColor = Color.TRANSPARENT
+
+        val login : Button = findViewById(R.id.login_button)
+        login.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
